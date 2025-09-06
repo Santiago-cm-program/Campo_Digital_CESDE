7947fbd9da36e65ef0568d08bff5029d8dee48b7
Santiago Correa <stillsantiago@gmail.com>
2025-09-06 13:29:32 -0500

    Configuración del endpoint(documentación)

--- src/main/java/com/example/pib2/Users/controller/UserController/UserController.java
+++ src/main/java/com/example/pib2/Users/controller/UserController/UserController.java
@@ -20,6 +20,8 @@ import com.example.pib2.Users.model.dto.UpdateUser.ClientUpdateDTO;
 import com.example.pib2.Users.model.dto.Users.ClientsDTO;
 import com.example.pib2.Users.service.ServiceUser.UserService;
 
+import io.swagger.v3.oas.annotations.Operation;
+
 @RestController
 @RequestMapping("v1/api/Users")
 public class UserController {
@@ -28,12 +30,14 @@ public class UserController {
     private UserService userService;
 
     @GetMapping()
+    @Operation(summary="Obtener todos los usuarios", description="End Point para obtener todos los usuarios de la base de datos")
     public ResponseEntity<List<ClientsDTO>> getTypeDocument() {
         List<ClientsDTO> Clients = userService.getAllClients();
         return new ResponseEntity<>(Clients, HttpStatus.OK);
     }
 
     @PostMapping("POST")
+    @Operation(summary="Crear un nuevo usuario",description="End Point para crear un usuario en la base de datos")  
     public ResponseEntity<Clientes> createNewClient(@RequestBody ClientsInsertDTO clienteInsert) {
         try {
             Clientes NewCliente = userService.createNewClient(clienteInsert);
@@ -45,6 +49,7 @@ public class UserController {
     }
 
     @PutMapping("PUT/{id}")
+    @Operation(summary="Modifica información de un usuario", description="End Point que te permite modificar información de un usuario, se debe buscar el usuario mediante el id")
     public ResponseEntity<Clientes> updateCliente(
             @PathVariable Long id,
             @RequestBody ClientUpdateDTO clientUpdate) {
@@ -54,6 +59,7 @@ public class UserController {
     }
 
     @PutMapping("PUT/{id}/activo")
+    @Operation(summary="Inactivar Usuario",description="End Point que te permite inactivar usuarios")
     public ResponseEntity<String> UpdateStatusCliente(@PathVariable("id") Long idCliente,
             @RequestParam("activo") boolean activo) {
 
@@ -66,12 +72,14 @@ public class UserController {
     }
 
     @GetMapping("GET/Documento/{numeroDocumento}")
+    @Operation(summary="Obtener usuario por numero de documentos",description="End Point que te permite obtener usuario por el numero del documento")
     public ResponseEntity<List<ClientsDTO>> getByNumberDocument(@PathVariable String numeroDocumento) {
         List<ClientsDTO> client = userService.getClientByNumeroDocumento(numeroDocumento);
         return ResponseEntity.ok(client);
     }
 
     @GetMapping("GET/IdCliente/{idCliente}")
+    @Operation(summary="Obtener el usuario por id",description="End Point para obtener el usuario por el id")
     public ResponseEntity<List<ClientsDTO>> findByIdCliente(@PathVariable Long idCliente) {
         List<ClientsDTO> client = userService.findByIdCliente(idCliente);
 

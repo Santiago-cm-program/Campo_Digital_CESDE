f7727617ab164e88dea4021e7b98c1a476615142
Santiago Correa <stillsantiago@gmail.com>
2025-10-20 22:41:01 -0500

    cambio en nombre de doker

--- src/main/java/com/example/pib2/config/OpenApiConfig.java
+++ src/main/java/com/example/pib2/config/OpenApiConfig.java
@@ -1,6 +1,20 @@
 package com.example.pib2.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
 public class OpenApiConfig {
 
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API Campo Digital CESDE")
+                        .version("1.0.0")
+                        .description("Documentación de la API para el proyecto Campo Digital CESDE"));
+    }
 }
